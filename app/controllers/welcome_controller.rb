0b96bcdc2538477065ce0e5de64b652ba4e92ee5
Nikolai Volochaev <nik@funriver.net>
2014-08-04 18:25:52 +0400

    up

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -18,7 +18,15 @@ module DX
         'css/lists.css',
         'css/forms.css',
         'css/buttons.css',
-        'css/header.css'
+        'css/header.css',
+        'css/footer.css',
+        'css/boxes_panels_modals.css',
+        'css/cart_checkout.css',
+        'css/categories.css',
+        'css/flexslider.css',
+        'css/products.css',
+        'css/ratings.css',
+        'css/tabs.css'
       ]
 
       serve '/images', from: root + '/assets/images'
